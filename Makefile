ef6767ca32ebceac7c0e6855d1db056c2c02662a
Vincent Pelletier <plr.vincent@gmail.com>
2011-06-03 18:15:44 +0000

    Variadic is as simple as not specifying argtypes.

--- Makefile
+++
@@ -1,4 +0,0 @@
-all:
-	gcc -fPIC -c devaarg.c
-	ld -shared -soname devaarg.so.1 -o devaarg.so.1.0 -lc devaarg.o
-
--- devaarg.c
+++
@@ -1,8 +0,0 @@
-typedef void *(*charp)(char *, ...);
-
-#define charp__(name, type)  void *name(charp func, char *a, type b) { return func(a, b); }
-charp__(charp__charp, char *)
-charp__(charp__int, int)
-charp__(charp__ulint, unsigned long int)
-charp__(charp__voidp, void *)
-
