aef7af8e1b5712007ed9782be5a1e85aea86166c
Vincent Pelletier <plr.vincent@gmail.com>
2013-01-06 22:12:30 +0100

    Make ib_status use same settings as other tests.

--- ib_status.py
+++ ib_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 import innodb
+import test0aux
 from pprint import pprint
 
 def main():
     idb = innodb.InnoDB()
+    test0aux.test_configure()
     idb.startup('barracuda')
 
     pprint(idb.getOptionDict())
